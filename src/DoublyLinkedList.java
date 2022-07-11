767bb4993ae36a7c43fc12cac4a26afda6125774
BurgerMeanie <kdbergamini@gmail.com>
2022-07-10 23:02:27 -0400

    Creation of Append Method, toString method, and associated test methods.

--- src/DoublyLinkedList.java
+++ src/DoublyLinkedList.java
@@ -1,5 +1,39 @@
-public class DoublyLinkedList {
+public class DoublyLinkedList<T extends Comparable<T>> {
+    Node<T> head;
     public DoublyLinkedList(){
+    }
+    public Node append(T data){
+        Node<T> toAppend = new Node<>(data);
 
+        if(this.head == null){
+            this.head = toAppend;
+            return this.head;
+        }
+
+        Node lastNode = this.head;
+        while(lastNode.next != null){
+            lastNode = lastNode.next;
+        }
+
+        lastNode.next = toAppend;
+
+        return toAppend;
+    }
+    @Override
+    public String toString(){
+        Node<T> toPrint = this.head;
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while(toPrint != null){
+            stringBuilder.append(toPrint.data);
+            stringBuilder.append(" -> ");
+
+            toPrint = toPrint.next;
+        }
+
+        stringBuilder.append("NULL");
+
+        return stringBuilder.toString();
     }
 }
--- src/DoublyLinkedListTest.java
+++ src/DoublyLinkedListTest.java
@@ -1,5 +1,17 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DoublyLinkedListTest {
 
+    @Test
+    void append() {
+        DoublyLinkedList dll = new DoublyLinkedList();
+        dll.append(1);
+        assertEquals(dll.toString(), "1 -> NULL");
+    }
+
+    @Test
+    void testToString() {
+    }
 }
